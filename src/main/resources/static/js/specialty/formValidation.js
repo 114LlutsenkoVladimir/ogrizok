4df5243f079ae8fe6e19829705fb23df8b7e1623
114lutsenkoladimir <lucenkovaldymar@gmail.com>
2025-07-29 16:33:34 +0300

    .

--- src/main/resources/static/js/specialty/formValidation.js
+++ src/main/resources/static/js/specialty/formValidation.js
@@ -6,4 +6,16 @@ export function checkFindSpecialtyForm() {
 
 export function checkUpdateSpecialtyPlacesForm() {
     checkEmptyInputs("updateSpecialtyPlaces")
+    const contractPlacesValue = document.getElementById("updatePlacesContractPlaces").value
+    const budgetPlacesValue = document.getElementById("updatePlacesBudgetPlaces").value
+
+    if (contractPlacesValue.trim() !== "") {
+        if (contractPlacesValue <= 0)
+            throw new Error("Кіль-ть контрактних місць не может бути <= 0")
+    }
+
+    if (budgetPlacesValue.trim() !== "") {
+        if (budgetPlacesValue < 0)
+            throw new Error("Кіль-ть бюджетних місць не может бути < 0")
+    }
 }
